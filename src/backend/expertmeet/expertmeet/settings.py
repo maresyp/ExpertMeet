c8330f11f09e0c93ca356aa4fc877266909ac161
maresyp <mattq.2000@icloud.com>
2024-03-12 11:06:45 +0100

    Update SECRET_KEY and print its value

--- src/backend/expertmeet/expertmeet/settings.py
+++ src/backend/expertmeet/expertmeet/settings.py
@@ -29,10 +29,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 dotenv.load_dotenv()
-SECRET_KEY = os.getenv("SECRET_KEY")
-print(SECRET_KEY)
 Python_ROOT_DIR = os.getenv("Python_ROOT_DIR")
 print(f"{Python_ROOT_DIR=}")
+SECRET_KEY = os.getenv("SECRET_KEY")
+print(SECRET_KEY)
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
