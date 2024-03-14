71f05962af284f88120945d9ca7c99106884e89a
maresyp <mattq.2000@icloud.com>
2024-03-14 13:01:04 +0100

    Add user registration functionality

---
+++ src/frontend/web/src/context/AlertContext.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const AlertContext = React.createContext();
+
+export default AlertContext;
+
+export const AlertProvider = ({ children }) => {
+    const [alert, setAlert] = React.useState({ open: false, message: '', severity: '' });
+    const navigate = useNavigate()
+
+    const showAlert = (message, severity, timeout = 3000) => {
+        setAlert({ open: true, message, severity });
+        setTimeout(() => setAlert({ open: false, message: '', severity: '' }), timeout);
+    };
+
+    const navigateWithAlert = (path, message, severity, timeout = 3000) => {
+        navigate(path);
+        showAlert(message, severity, timeout);
+    };
+
+    let contextData = {
+        alert: alert,
+        showAlert: showAlert,
+        navigateWithAlert: navigateWithAlert,
+    }
+
+    return (
+        <AlertContext.Provider value={contextData}>
+            {children}
+        </AlertContext.Provider>
+    );
+};
---
+++ src/frontend/web/src/pages/SignUp.js
@@ -0,0 +1,144 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import AlertContext from '../context/AlertContext';
+import Alert from '@mui/material/Alert';
+
+function Copyright(props) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://github.com/maresyp/ExpertMeet">
+                ExpertMeet
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+export default function SignUp() {
+    const { alert, navigateWithAlert, showAlert } = React.useContext(AlertContext)
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const form_data = new FormData(event.currentTarget);
+        const first_name = form_data.get('firstName')
+        const last_name = form_data.get('lastName')
+        const login = form_data.get('email')
+        const pwd = form_data.get('password')
+
+        // TODO : add validation of user input
+        // TODO : add confirm password option ( second password )
+
+        let response = await fetch(
+            'http://127.0.0.1:8080/api/register/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ 'username': login, 'password': pwd, "first_name": first_name, "last_name": last_name, "email": login })
+        })
+
+        if (response.status === 201) {
+            navigateWithAlert('/login', 'Utworzono nowe konto, wprowadź swoje dane.', 'success', 10000)
+        } else {
+            // TODO: This need error handling ( network errors )
+            const data = await response.json();
+            console.log(data);
+            showAlert('Nie udało się utworzyć konta', 'error')
+        }
+    };
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Zarejestruj się
+                </Typography>
+                {alert.open && <Alert severity={alert.severity}>{alert.message}</Alert>}
+                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                autoComplete="given-name"
+                                name="firstName"
+                                required
+                                fullWidth
+                                id="firstName"
+                                label="Imię"
+                                autoFocus
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="lastName"
+                                label="Nazwisko"
+                                name="lastName"
+                                autoComplete="family-name"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email"
+                                name="email"
+                                autoComplete="email"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                name="password"
+                                label="Hasło"
+                                type="password"
+                                id="password"
+                                autoComplete="new-password"
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Zarejestruj
+                    </Button>
+                    <Grid container justifyContent="flex-end">
+                        <Grid item>
+                            <Link href="/login" variant="body2">
+                                Masz już konto ? Zaloguj się
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+            <Copyright sx={{ mt: 5 }} />
+        </Container>
+    );
+}
