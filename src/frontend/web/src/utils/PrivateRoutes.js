ba702cfbe1cb1b6f9a926d99e0ea78197c6e9f4d
maresyp <mattq.2000@icloud.com>
2024-03-15 17:47:00 +0100

    Update alert timeout and add error message for unauthorized access

--- src/frontend/web/src/utils/PrivateRoutes.js
+++ src/frontend/web/src/utils/PrivateRoutes.js
@@ -1,9 +1,18 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Outlet, Navigate } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
+import AlertContext from "../context/AlertContext";
 
 const PrivateRoutes = () => {
     let { user } = useContext(AuthContext)
+    let { showAlert } = useContext(AlertContext)
+
+    useEffect(() => {
+        if (!user) {
+            showAlert("Ta strona jest dostępna tylko dla zalogowanych użytkowników.", "error")
+        }
+    }, [user, showAlert])
+
     return (
         user ? <Outlet /> : <Navigate to="/login" />
     )
