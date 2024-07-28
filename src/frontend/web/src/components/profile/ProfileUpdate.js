6c08bac561ec1dbd3263490781855cf21a06ecfe
maresyp <mattq.2000@icloud.com>
2024-07-28 12:32:13 +0200

    add files structure for profile view

---
+++ src/frontend/web/src/components/profile/ProfileUpdate.js
@@ -0,0 +1,19 @@
+import Alert from '@mui/material/Alert';
+import AlertContext from '../../context/AlertContext';
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import * as React from 'react'
+
+function ProfileUpdate() {
+    const { alert, showAlert } = React.useContext(AlertContext)
+    const handleSubmit = () => {
+
+    }
+
+    return (
+        <div>test</div>
+    )
+}
+
+
+export default ProfileUpdate;
--- src/frontend/web/src/pages/MyProfile.js
+++ src/frontend/web/src/pages/MyProfile.js
@@ -9,18 +9,39 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import Divider from '@mui/material/Divider';
-import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
 import Avatar from '@mui/material/Avatar';
-import Fab from '@mui/material/Fab';
-import SendIcon from '@mui/icons-material/Send';
-import CallIcon from '@mui/icons-material/Call';
-import { Badge, IconButton, Tooltip } from '@mui/material';
+import PropTypes from 'prop-types';
+import ProfileUpdate from '../components/profile/ProfileUpdate';
+
+function CustomTabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+        </div>
+    );
+}
+
+CustomTabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
 
 const MyProfile = () => {
     const { authTokens } = React.useContext(AuthContext)
@@ -99,18 +120,24 @@ const MyProfile = () => {
                     </Grid>
                     <Grid item xs={9} sx={{ display: 'flex', flexDirection: 'column', height: '700px' }}>
                         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '35px', maxHeight: '65px' }}>
-                            <Box sx={{ width: '100%' }}>
                                 <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                                     <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                                        <Tab label="Item One" />
-                                        <Tab label="Item Two" />
-                                        <Tab label="Item Three" />
-                                    </Tabs>
-                                </Box>
+                                    <Tab label="informacje" {...a11yProps(0)} />
+                                    <Tab label="harmonogram" {...a11yProps(1)} />
+                                    <Tab label="Item Three" {...a11yProps(2)} />
+                                </Tabs>
                             </Box>
                         </Box>
                         <Box sx={{ overflow: 'auto', flex: 1 }}>
-
+                            <CustomTabPanel value={value} index={0}>
+                                <ProfileUpdate />
+                            </CustomTabPanel>
+                            <CustomTabPanel value={value} index={1}>
+                                Item Two
+                            </CustomTabPanel>
+                            <CustomTabPanel value={value} index={2}>
+                                Item Three
+                            </CustomTabPanel>
                         </Box>
                         <Grid container style={{ padding: '20px' }}></Grid>
                     </Grid>
